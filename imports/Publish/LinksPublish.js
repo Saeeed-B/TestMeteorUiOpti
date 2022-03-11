4cac67e4bc9032cea3defb9ff533f78c8a38d303
Frederico Maia Arantes <fredmaiaarantes@gmail.com>
2022-03-11 09:00:36 -0300

    Sample of how to fetch data with Meteor Methods and keep it on the client using useState

--- imports/Publish/LinksPublish.js
+++ imports/Publish/LinksPublish.js
@@ -1,5 +1,7 @@
 import { Meteor } from "meteor/meteor";
 import {LinksCollection}  from "../api/Link";
+
+// It could be removed as it is not used anymore
 Meteor.publish('AllLinksPublish', function() {
     return LinksCollection.find({});
 });
